3fcf7fb741c2c21a94bb8f9c57c7e22f78748a82
jacobgellman <gellman@bren.ucsb.edu>
2019-07-06 22:23:32 -0400

    More data cleaning.

--- generate_maps.R
+++ generate_maps.R
@@ -21,12 +21,6 @@ CPI <- read_xlsx("raw_data/CPI_CUUR0000SA0.xlsx",range="A12:P23")
 # inflation from 1997-2007
 inflation_97_07 <- (CPI$Annual[length(CPI$Annual)] - CPI$Annual[1])/CPI$Annual[1]
 
-# discount rate
-discount <- 0.05
-
-# social cost of carbon
-carbon_cost <- 12
-
 # read in forest conversion data (Nielsen et al. 2013)
 forest_conversion <- read_xlsx("raw_data/county-level-data_nielsen2013.xlsx",range="A11:O3080")
 colnames(forest_conversion) <- c("FIPS","county",
@@ -39,10 +33,10 @@ colnames(forest_conversion) <- c("FIPS","county",
 # source: http://www.fs.fed.us/pnw/pubs/pnw_gtr888/county-level-data_nielsen2013.xlsx
 
 # adjust from 1997 dollars to 2007 dollars
-forest_conversion_inflated <- forest_conversion  # a temporary df
+forest_conversion_inflated <- forest_conversion  # a temporary df for inflation calculations
 for (i in 3:10) {
   forest_conversion_inflated[,i] <- forest_conversion[,i]*(1+inflation_97_07)  # inflation calculation
-}
+  }
 colnames(forest_conversion_inflated)[3:10] <-
   paste0(colnames(forest_conversion)[3:10],"_inflated")
 forest_conversion <- left_join(
@@ -56,10 +50,67 @@ rm(forest_conversion_inflated)  # removing the temporary inflated df
 # calculate opportunity cost
 forest_conversion <- forest_conversion %>%
   mutate(
-    opp_cost_wo_harvest_crop = land_price_wo_harvest_crop_inflated + tree_establishment_CRP_predicted_inflated,
-    opp_cost_wo_harvest_pasture = land_price_wo_harvest_pasture_inflated + tree_establishment_CRP_predicted_inflated,
-    opp_cost_wo_harvest_range = land_price_wo_harvest_range_inflated + tree_establishment_CRP_predicted_inflated,
-    opp_cost_w_harvest_crop = land_price_w_harvest_crop_inflated + tree_establishment_CRP_predicted_inflated,
-    opp_cost_w_harvest_pasture = land_price_w_harvest_pasture_inflated + tree_establishment_CRP_predicted_inflated,
-    opp_cost_w_harvest_range = land_price_w_harvest_range_inflated + tree_establishment_CRP_predicted_inflated
+    opp_cost_wo_harvest_crop = sum(land_price_wo_harvest_crop_inflated, tree_establishment_CRP_predicted_inflated, na.rm=TRUE),
+    opp_cost_wo_harvest_pasture = sum(land_price_wo_harvest_pasture_inflated, tree_establishment_CRP_predicted_inflated, na.rm=TRUE),
+    opp_cost_wo_harvest_range = sum(land_price_wo_harvest_range_inflated, tree_establishment_CRP_predicted_inflated, na.rm=TRUE),
+    opp_cost_w_harvest_crop = sum(land_price_w_harvest_crop_inflated, tree_establishment_CRP_predicted_inflated, na.rm=TRUE),
+    opp_cost_w_harvest_pasture = sum(land_price_w_harvest_pasture_inflated, tree_establishment_CRP_predicted_inflated, na.rm=TRUE),
+    opp_cost_w_harvest_range = sum(land_price_w_harvest_range_inflated, tree_establishment_CRP_predicted_inflated, na.rm=TRUE)
     )
+
+# calculate MC = opp_cost/MT of C
+forest_conversion <- forest_conversion %>%
+  mutate(
+    mc_wo_harvest_crop = ifelse(is.finite(opp_cost_wo_harvest_crop/C_uptake_wo_harvest),opp_cost_wo_harvest_crop/C_uptake_wo_harvest,NA),
+    mc_wo_harvest_pasture = ifelse(is.finite(opp_cost_wo_harvest_pasture/C_uptake_wo_harvest),opp_cost_wo_harvest_pasture/C_uptake_wo_harvest,NA),
+    mc_wo_harvest_range = ifelse(is.finite(opp_cost_wo_harvest_range/C_uptake_wo_harvest),opp_cost_wo_harvest_range/C_uptake_wo_harvest,NA),
+    mc_w_harvest_crop = ifelse(is.finite(opp_cost_w_harvest_crop/C_uptake_w_harvest),opp_cost_w_harvest_crop/C_uptake_w_harvest,NA),
+    mc_w_harvest_pasture = ifelse(is.finite(opp_cost_w_harvest_pasture/C_uptake_w_harvest),opp_cost_w_harvest_pasture/C_uptake_w_harvest,NA),
+    mc_w_harvest_range = ifelse(is.finite(opp_cost_w_harvest_range/C_uptake_w_harvest),opp_cost_w_harvest_range/C_uptake_w_harvest,NA)
+    )
+
+# afforested acres under a $12/t carbon price
+forest_conversion <- forest_conversion %>%
+  mutate(
+    crop_acres_12 = ifelse(is.na(mc_wo_harvest_crop),NA,
+                            ifelse(mc_wo_harvest_crop > 12, 0,
+                                   land_eligible_conversion_crop)),
+    pasture_acres_12 = ifelse(is.na(mc_wo_harvest_pasture),NA,
+                            ifelse(mc_wo_harvest_pasture > 12, 0,
+                                   land_eligible_conversion_pasture)),
+    range_acres_12 = ifelse(is.na(mc_wo_harvest_range),NA,
+                            ifelse(mc_wo_harvest_range > 12, 0,
+                                   land_eligible_conversion_range)),
+    total_acres_12 = sum(crop_acres_12,pasture_acres_12,range_acres_12,na.rm=TRUE)
+  )
+
+# afforested acres under a $20/t carbon price
+forest_conversion <- forest_conversion %>%
+  mutate(
+    crop_acres_20 = ifelse(is.na(mc_wo_harvest_crop),NA,
+                           ifelse(mc_wo_harvest_crop > 20, 0,
+                                  land_eligible_conversion_crop)),
+    pasture_acres_20 = ifelse(is.na(mc_wo_harvest_pasture),NA,
+                              ifelse(mc_wo_harvest_pasture > 20, 0,
+                                     land_eligible_conversion_pasture)),
+    range_acres_20 = ifelse(is.na(mc_wo_harvest_range),NA,
+                            ifelse(mc_wo_harvest_range > 20, 0,
+                                   land_eligible_conversion_range)),
+    total_acres_20 = sum(crop_acres_20,pasture_acres_20,range_acres_20,na.rm=TRUE)
+  )
+
+# afforested acres under a $50/t carbon price
+forest_conversion <- forest_conversion %>%
+  mutate(
+    crop_acres_50 = ifelse(is.na(mc_wo_harvest_crop),NA,
+                           ifelse(mc_wo_harvest_crop > 50, 0,
+                                  land_eligible_conversion_crop)),
+    pasture_acres_50 = ifelse(is.na(mc_wo_harvest_pasture),NA,
+                              ifelse(mc_wo_harvest_pasture > 50, 0,
+                                     land_eligible_conversion_pasture)),
+    range_acres_50 = ifelse(is.na(mc_wo_harvest_range),NA,
+                            ifelse(mc_wo_harvest_range > 50, 0,
+                                   land_eligible_conversion_range)),
+    total_acres_50 = sum(crop_acres_50,pasture_acres_50,range_acres_50,na.rm=TRUE)
+  )
+
